dd23c81ac8acfc278d4389147b4a26cf62badc67
Trimesh-fdo <fernando.chamal@yahoo.com>
2026-05-15 01:35:22 +0530

    Delete backend directory

--- backend/routes/chatbot.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { chat } = require('../controllers/chatbotController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, chat);
-
-module.exports = router;
--- backend/routes/workout.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { logWorkout, getWorkouts } = require('../controllers/workoutController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', protect, logWorkout);
-router.get('/',  protect, getWorkouts);
-
-module.exports = router;
